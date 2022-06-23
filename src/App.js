6832f6f553aa7646922a3f03e3123be935a426cb
Hossam <Matrixhoss2@gmail.com>
2022-06-23 15:23:48 +0200

    init test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./components/Form";
+import List from "./components/List";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>YDS - Task</h1>
+
+      <Form />
+
+      <div className='divider' />
+      <List />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = () => {
+  return <div>Please write your Form code here</div>;
+};
+
+export default Form;
